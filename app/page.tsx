f90271fc9a04a389a7b65c7224c0539ff98c447f
hluandev <97277916+hluandev@users.noreply.github.com>
2024-03-31 01:50:30 -0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,9 @@ export default function Home() {
       <p>
         Much of the art I appreciate in museums can be bucketed into aesthetics
         like typographic, medieval, or patterned. The boundaries feel clearly
-        defined because we can assign names to them. I have been increasingly
-        more stimulated by aesthetic intersections—unexpected displays of art
-        that tastefully reject the notion of a clean, singularly defined style,
-        yet bridges many. For my taste, often this ends up in opposition to
-        minimalism, yet does not chaotically lean into kitschy or maximalism.
-        There is authoring intent, purpose, and a sense of iteration felt
-        throughout, not seemingly arbitrary excess for the sake of creative
-        provocation. I believe there's something fascinating in art and design
-        that treats my attention with care and instills curiosity.
       </p>
+      <p>Love is here</p>
+      <p>Dont worry about it</p>
     </main>
   );
 }
