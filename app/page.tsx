9b4148aa719f0f987204d51dc94f45ec57374278
Luan <97277916+hluandev@users.noreply.github.com>
2024-04-07 11:42:13 -0700

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
 export default function Home() {
   return (
     <main className="text-green-400">
-      <h1>This is kinda nice</h1>
-      <p>This is kinda cool</p>
-      <p>Love is here</p>
-      <p>Love is kind here</p>
+      <h1>Wow you are cool</h1>
     </main>
   );
 }
