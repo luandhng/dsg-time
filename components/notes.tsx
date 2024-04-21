177c821adecc0975004a9ff6134f2d28d90444c9
Luan <97277916+hluandev@users.noreply.github.com>
2024-04-21 00:52:18 -0700

    updated

--- components/notes.tsx
+++ components/notes.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { updateNoteServer } from "@/actions/updateNoteServer";
+import { updateTopicServer } from "@/actions/updateTopicServer";
 import { useNotesDataStore } from "@/stores/notesDataStore";
 import { useSelectedTopicStore } from "@/stores/selectedTopicStore";
 
@@ -14,7 +16,13 @@ export const Notes = () => {
           item.this_topic === selectedTopic && (
             <div key={index} className="flex flex-col h-full">
               <input
-                onChange={(e) => updateTopic(item.this_topic, e.target.value)}
+                onChange={(e) => {
+                  updateTopic(item.this_topic, e.target.value);
+                  updateTopicServer({
+                    newTopic: e.target.value,
+                    this_topic: item.this_topic,
+                  });
+                }}
                 defaultValue={item.topic}
                 value={item.topic}
                 className="focus:outline-none p-3 border-b border-neutral-400 font-semibold"
@@ -23,6 +31,10 @@ export const Notes = () => {
               <textarea
                 onChange={(e) => {
                   updateNote(item.this_topic, e.target.value);
+                  updateNoteServer({
+                    newNote: e.target.value,
+                    this_topic: item.this_topic,
+                  });
                 }}
                 className="p-3 flex-1 h-full focus:outline-none"
                 defaultValue={item.note}
